86940b8791ed75b4c1a883ac2d2500baca88b11a
abhamud18 <abhamud18@gmail.com>
2022-04-07 18:33:30 -0700

    asdfhakshjasfd

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,20 +1,19 @@
-/*this program will run some sort of print common guides 
- *
- *
- *
- *
- * 
+/*
+ * File: 5-printf.c
+ * Auth: Brennan D Baraban
  */
+
 #include <stdio.h>
 
-/*
- * main - prints this fiile 
- * this will be our main programing
- * Returns ALways 0 
+/**
+ * main - Prints "with proper grammar, but the outcome is
+ *                a piece of art," followed by a new line.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
 
+	return (0);
 }
