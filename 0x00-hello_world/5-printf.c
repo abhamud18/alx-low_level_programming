dc89d87ab3e1714ae3e30221b7ab8a055442e8e6
abhamud18 <abhamud18@gmail.com>
2022-04-07 18:27:37 -0700

    asdkghkhajsdf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+/*this program will run some sort of print common guides 
+ *
+ *
+ *
+ *
+ * 
+ */
+#include <stdio.h>
+
+/*
+ * this will be our main programing 
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+
+}
