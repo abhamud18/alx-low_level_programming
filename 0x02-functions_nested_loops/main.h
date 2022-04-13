8d6dc57e727e975b95c35acea9b6f10a15cd33d6
abhamud18 <abhamud18@gmail.com>
2022-04-12 19:50:52 -0700

    asdfkhajhsdf

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,3 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
-
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -9,10 +6,7 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
-void jack_bawer(void);
+void jack_bauer(void);
 void times_table(void);
-int add(int ,int);
+int add(int, int);
 void print_to_98(int n);
-void print_times_table(int n);
-#endif /* _MAIN_H_*/
-
