952acdefb1f0e25768bf1b96069f133c403fb80c
= <=>
2022-05-19 13:30:45 -0700

    asdfhlahjsdhlsd

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,25 +2,25 @@
 #define LISTS_H
 
 /**
- * struct list_sin - singly linked list
- * @strn: string - (malloc'ed string)
- * @leng: length of the string
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
  * @next: points to the next node
  *
- * Description: singly linked list node structure
+ * Description: singly linked list node strucutre
  */
-typedef struct list_sin
+typedef struct list_s
 {
 	char *str;
-	unsigned int leng;
+	unsigned int len;
 	struct list_s *next;
-} list_ab;
+} list_t;
 int _putchar(char c);
-size_ab print_list(const list_ab *h);
-size_ab list_len(const list_ab *h);
-list_ab *add_node(list_ab **head, const char *str);
-list_ab *add_node_end(list_ab **head, const char *str);
-void free_list(list_ab *head);
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 void __attribute__((constructor)) calledFirst();
 
 #endif
