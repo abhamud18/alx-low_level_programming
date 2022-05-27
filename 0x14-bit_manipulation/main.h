e4a4f485f68c22d63d750b79b5720ff6e34852f8
= <=>
2022-05-27 16:11:21 -0700

    Update

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,5 +1,12 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ * AUth: Ismail Mohamed
+ * Desc: Header file containing prototypes for all functions
+ *       written in the 0x13-bit_manipulation directory.
+ */
 
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
@@ -10,4 +17,6 @@ int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
 
-#endif
+#endif /* MAIN_H */
+© 2022 GitHub, Inc.
+Term
